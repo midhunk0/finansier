5fd29c8ad5140bd069203ecc78db19ff5ea40de4
midhunrajk <midhunrajk009@gmail.com>
2023-05-12 16:24:03 +0530

    adding front-end

---
+++ src/expanded-theme.ts
@@ -0,0 +1,11 @@
+// eslint-disable
+import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
+
+declare module "@mui/material/styles/createPalette"{
+    interface PaletteColor{
+        [key: number]:string;
+    }
+    interface Palette{
+        tertiary:PaletteColor;
+    }
+}
